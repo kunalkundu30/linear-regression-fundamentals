79faa493039a3b8c47fb2aeacb00c5a74a35b989
kunalkundu30 <kk3585@columbia.edu>
2024-04-15 20:42:55 -0400

    sample generation

---
+++ utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+from typing import Tuple
+
+
+def generate_samples_linear_reg(
+    samples: int,
+    features: int,
+    lower_bound: int,
+    upper_bound: int,
+    intercept: int,
+    coeffecients: np.array,
+) -> Tuple[np.array, np.array]:
+    """
+    Return samples and label from target linear regression distribution.
+
+    Parameters
+    ----------
+    samples: int
+        Number of samples to be sampled
+    features: int
+        Number of features
+    lower_bound: int
+        Lower bound on values of all features
+    upper_bound: int
+        Upper bound on values of all features
+    intercept: int
+        Intercept in the linear regression target distribution
+    coeffecients: np.array
+        The coeffecients of features in linear regression target distribution
+
+    Returns
+    -------
+    X: np.array
+        Generated samples
+    y: np.array
+        Generated labels from the distribution
+
+    """
+    X = np.empty((samples, features))
+    y = np.empty(samples)
+    for sample in range(samples):
+        for feature in range(features):
+            X[sample][feature] = np.random.uniform(lower_bound, upper_bound, size=1)
+    y = np.dot(X, coeffecients) + intercept
+
+    return (X, y)
